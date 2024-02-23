929686ea044f992dcd7eccc25b1c43247be75727
crptmem <corrupt@kijy.de>
2024-02-23 13:16:57 +0300

    add more documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+/// API calls
 pub mod request;
 
+/// This crate provides asynchronous functions to access Boosty not documented, but accessible API
 #[cfg(test)]
 mod tests {
     use crate::request;
--- src/request.rs
+++ src/request.rs
@@ -5,7 +5,8 @@ use serde_json::Value;
 use serde::{Deserialize, Serialize};
 use error_chain::error_chain;
 
-const API_URL: &str = "https://api.boosty.to";
+/// Boosty API URL
+pub const API_URL: &str = "https://api.boosty.to";
 
 error_chain! {
     foreign_links {
@@ -16,8 +17,11 @@ error_chain! {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "UPPERCASE")]
+/// Prices of a paid post in two currencies
 pub struct CurrencyPrices {
+    /// Price in russian rubles
     pub rub: f64,
+    /// Price in american dollars
     pub usd: f64
 }
 
@@ -25,26 +29,42 @@ pub struct CurrencyPrices {
 #[serde(rename_all = "camelCase")]
 pub struct Teaser {
     #[serde(rename = "type")]
+    /// Type of teaser
     pub ctype: String,
+    /// Width of content
     pub width: Option<isize>,
+    /// Height of content
     pub height: Option<isize>,
+    /// Rendition of content
     pub rendition: Option<String>,
+    /// URL of content
     pub url: Option<String>,
+    /// Teaser ID
     pub id: Option<String>
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
+/// Boosty post on a blog
 pub struct Post {
+    /// Creation time in Unix format
     pub created_at: u64,
+    /// Update time in Unix format
     pub updated_at: Option<u64>,
+    /// Publish time in Unix format
     pub publish_time: u64,
 
+    /// Paid post price in two currencies
     pub currency_prices: CurrencyPrices,
+    /// Teaser of paid post
     pub teaser: Vec<Teaser>,
+    /// Is post views counter visible
     pub show_views_counter: bool,
+    /// Price of a paid post (0 if free)
     pub price: isize,
+    /// Post ID
     pub id: String,
+    /// Post title
     pub title: String
 }
 
